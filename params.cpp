f9d25b394596b67c60e42624332b037f35cff568
nelavara <evanmperry@gmail.com>
2020-09-08 21:45:42 -0400

    more teasing out to see if we can get everything in data container. Not sure if I am going about this write but I think its a good start. We need to get data ready for getopt_long

--- params.cpp
+++ params.cpp
@@ -21,15 +21,51 @@ ostream & params::print(ostream &out) {
 
 void params::parseCommands(int, char **) {
     cout << "------------------------------------\n";
-    cout << "command  " << stringIn[0] << '\n';
+    const char* command = stringIn [0]; //not sure if this needs be const or not refer
+    //to https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Options.html
+    cout << "command   " << command << '\n';
+    string smallSwitch;
+    string largeSwitch;
+    string argument;
+    string largeArgument;
+    bool ss = false;
+    bool ls = false;
 
     for (int i=1; i< size; ++i) {
-        if(stringIn[i][0] == '-')
-            cout << "switch    " << stringIn[i] <<'\n';
-        else if((stringIn[i][0] == '-' ) && (stringIn[i][1] == '-'))
-            cout << "long option " << stringIn[i] << '\n';
-        else
-            cout << "argument  " << stringIn[i] << '\n';
+        if(stringIn[i][0] == '-' && stringIn[i][1] != '-'){
+            ss = true;
+            ls = false;
+            smallSwitch.append(stringIn[i]);
+        }
+
+        else if((stringIn[i][0] == '-' ) && (stringIn[i][1] == '-')){
+            ss = false;
+            ls = true;
+            largeSwitch.append(stringIn[i]);
+        }
+        else{
+            if (ss){
+                argument.append(stringIn[i]);
+            }
+            if (ls){
+                largeArgument.append(stringIn[i]);
+            }
+        }
+
     }
+    cout << "switch    " << smallSwitch <<'\n';
+    cout << "verbose    " << largeSwitch <<'\n';
+    if (argument.empty){
+        cout << "argument  " << largeArgument << '\n';
+    }
+    else if (largeArgument.empty()){
+        cout << "argument  " << argument << '\n';
+    }
+    else{
+        cout << "argument  " << argument << '\n';
+        cout << "argument  " << largeArgument << '\n';
+    }
+
+
     cout << "--------------------------------------\n";
 }
