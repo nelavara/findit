38ddfbc7011bf22c7dde9036c0ec66b927cf6e8e
Jaron Bialecki <jaronb@Jarons-MacBook-Pro.local>
2020-10-15 14:07:37 -0400

    Fixed onefile functions comparison for sniffwords.

--- FileID.hpp
+++ FileID.hpp
@@ -24,7 +24,7 @@ class FileID {
 
         bool swrdEmpty() { return sniffWords.empty(); }
 
-        void insertSniff(string word) {     //vector<string>::iterator it;
+        void insertSniff(string word) {
             auto it = find(sniffWords.begin(), sniffWords.end(), word);
             if(it == sniffWords.end()) sniffWords.push_back(word);
         }
--- Sniff.cpp
+++ Sniff.cpp
@@ -64,7 +64,8 @@ oneFile(Dirent* dnt, Stats sts, string path) {
 
         // Compare each sniffword to the word found in the file
         for(string s : sniffWords) {    
-            if(s.compare(word)) fid.insertSniff(s);
+            if(s == word) 
+                fid.insertSniff(s);
         }
     }
 
